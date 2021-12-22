8ac08779bd9b87cb091683701b1a76dd6e9a1234
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2021-12-22 19:00:59 +0100

    Add Adjustment Interface to Godot

--- src/gdmath.cpp
+++ src/gdmath.cpp
@@ -11,39 +11,93 @@ GDMath::~GDMath() {
 }
 
 void GDMath::_register_methods() {
-    register_method("_process", &GDMath::_process);
-    register_property<GDMath, float>("r", &GDMath::r, 1.0);
-	register_property<GDMath, float>("e", &GDMath::e, 1.0);
-    register_property<GDMath, float>("speed", &GDMath::set_speed, &GDMath::get_speed, 1.0);
+	register_method("_ready", &GDMath::_ready);
+	register_method("_process", &GDMath::_process);
+	
+	register_property<GDMath, float>("speed", &GDMath::set_speed, &GDMath::get_speed, GDMATH_DEFAULT_SPEED);
+	register_property<GDMath, float>("radius", &GDMath::set_radius, &GDMath::get_radius, GDMATH_DEFAULT_RADIUS);
+	register_property<GDMath, float>("eccentricity", &GDMath::set_eccentricity, &GDMath::get_eccentricity, GDMATH_DEFAULT_ECCENTRICITY);
+	register_property<GDMath, Vector3>("centre", &GDMath::set_center, &GDMath::get_center, GDMATH_DEFAULT_CENTER);
+	register_property<GDMath, Vector3>("normal", &GDMath::set_normal, &GDMath::get_normal, GDMATH_DEFAULT_NORMAL);
+	register_property<GDMath, Vector3>("tangent", &GDMath::set_tangent, &GDMath::get_tangent, GDMATH_DEFAULT_TANGENT);
 }
 
 void GDMath::_init() {
-    // initialize any variables here
-    time_passed = 0.0;
-	speed = 1.0;
-    r = e = last_r = last_e = 1.0;
-	ellipse = new Ellipse(r, e, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+	ellipse = NULL;
+	time_passed = 0.0;
+	
+	speed = GDMATH_DEFAULT_SPEED;
+	radius = GDMATH_DEFAULT_RADIUS;
+	eccentricity = GDMATH_DEFAULT_ECCENTRICITY;
+	center = GDMATH_DEFAULT_CENTER;
+	normal = GDMATH_DEFAULT_NORMAL;
+	tangent = GDMATH_DEFAULT_TANGENT;
+}
+
+void GDMath::_ready() {
+	create_ellipse();
 }
 
 void GDMath::_process(float delta) {
-    time_passed += speed * delta;
-	if (r != last_r || e != last_e) {
-		last_r = r;
-		last_e = e;
-		delete ellipse;
-		ellipse = new Ellipse(r, e, 1, 1, 1, 1, 1, 1, 1, 1, 1);
-	}
-	
+	time_passed += speed * delta;
 	vector<float> v = ellipse->eval(time_passed);
-	Vector3 new_position = Vector3(v[0], v[1], v[2]);
+	set_translation( Vector3(v[0], v[1], v[2]) );
+}
 
-    set_translation(new_position);
+void GDMath::create_ellipse() {
+	ellipse = new Ellipse(radius, eccentricity, center.x, center.y, center.z, tangent.x, tangent.y, tangent.z, normal.x, normal.y, normal.z);
+}
+void GDMath::recreate_ellipse() {
+	if (ellipse) {
+		delete ellipse;
+		ellipse = new Ellipse(radius, eccentricity, center.x, center.y, center.z, tangent.x, tangent.y, tangent.z, normal.x, normal.y, normal.z);
+	}
 }
 
 void GDMath::set_speed(float p_speed) {
-    speed = p_speed;
+	speed = p_speed;
+	recreate_ellipse();
+}
+float GDMath::get_speed() {
+	return speed;
 }
 
-float GDMath::get_speed() {
-    return speed;
+void GDMath::set_radius(float p_radius) {
+	radius = p_radius;
+	recreate_ellipse();
+}
+float GDMath::get_radius() {
+	return radius;
+}
+
+void GDMath::set_eccentricity(float p_eccentricity) {
+	eccentricity = p_eccentricity;
+	recreate_ellipse();
+}
+float GDMath::get_eccentricity() {
+	return eccentricity;
+}
+
+void GDMath::set_center(Vector3 p_center) {
+	center = p_center;
+	recreate_ellipse();
+}
+Vector3 GDMath::get_center() {
+	return center;
+}
+
+void GDMath::set_normal(Vector3 p_normal) {
+	normal = p_normal;
+	recreate_ellipse();
+}
+Vector3 GDMath::get_normal() {
+	return normal;
+}
+
+void GDMath::set_tangent(Vector3 p_tangent) {
+	tangent = p_tangent;
+	recreate_ellipse();
+}
+Vector3 GDMath::get_tangent() {
+	return tangent;
 }
--- src/gdmath.h
+++ src/gdmath.h
@@ -5,28 +5,57 @@
 #include <Spatial.hpp>
 #include <ellipse.hpp>
 
+#define GDMATH_DEFAULT_SPEED 1.0
+#define GDMATH_DEFAULT_RADIUS 1.0
+#define GDMATH_DEFAULT_ECCENTRICITY 1.0
+#define GDMATH_DEFAULT_CENTER Vector3(1.0, 1.0, 1.0)
+#define GDMATH_DEFAULT_NORMAL Vector3(1.0, 1.0, 1.0)
+#define GDMATH_DEFAULT_TANGENT Vector3(1.0, 1.0, 1.0)
+
 namespace godot {
 
 class GDMath : public Spatial {
-    GODOT_CLASS(GDMath, Spatial)
+	GODOT_CLASS(GDMath, Spatial)
 
 private:
-    float time_passed;
-	float speed;
-    float r, e;
-	float last_r, last_e;
+	static const float DEFAULT_SPEED;
+	static const float DEFAULT_RADIUS;
+	static const float DEFAULT_ECCENTRICITY;
+	static const Vector3 DEFAULT_CENTER;
+	static const Vector3 DEFAULT_NORMAL;
+	static const Vector3 DEFAULT_TANGENT;
+	
 	Ellipse* ellipse;
-
+	float time_passed;
+	
+	float speed;
+	float radius, eccentricity;
+	Vector3 center, normal, tangent;
 public:
-    static void _register_methods();
+	static void _register_methods();
 
-    GDMath();
-    ~GDMath();
+	GDMath();
+	~GDMath();
 
-    void _init();
-    void _process(float delta);
-    void set_speed(float p_speed);
-    float get_speed();
+	void _init();
+	void _ready();
+	void _process(float delta);
+	
+	void create_ellipse();
+	void recreate_ellipse();
+	
+	void set_speed(float p_speed);
+	float get_speed();
+	void set_radius(float p_radius);
+	float get_radius();
+	void set_eccentricity(float p_eccentricity);
+	float get_eccentricity();
+	void set_center(Vector3 p_center);
+	Vector3 get_center();
+	void set_normal(Vector3 p_normal);
+	Vector3 get_normal();
+	void set_tangent(Vector3 p_tangent);
+	Vector3 get_tangent();
 };
 
 }
