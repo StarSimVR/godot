c77b6f9b7941e0ae83932ab6b3a6f36be564b9f6
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2022-01-09 20:33:35 +0100

    Update camera

--- project/scenes/3d/camera.gd
+++ project/scenes/3d/camera.gd
@@ -1,27 +1,31 @@
 extends Camera
 
 export var speed := 5.0
+export var speed_with_shift := 25.0
 export var look_sensitivity := 1.0
 var _mouse_motion := Vector2()
 
 func _process(delta: float) -> void:
 	transform.basis = Basis(Vector3(_mouse_motion.y * -0.001, _mouse_motion.x * -0.001, 0))
 
-	var input := Vector2()
+	var input := Vector3()
 	if Input.is_action_pressed("move_forward"):
-		input.y -= 1
+		input.z -= 1
 	if Input.is_action_pressed("move_backward"):
-		input.y += 1
+		input.z += 1
 	if Input.is_action_pressed("move_left"):
 		input.x -= 1
 	if Input.is_action_pressed("move_right"):
 		input.x += 1
+	if Input.is_action_pressed("move_up"):
+		input.y += 1
+	if Input.is_action_pressed("move_down"):
+		input.y -= 1
 	input = input.normalized()
 
-	var forward := global_transform.basis.z
-	var right := global_transform.basis.x
-	var direction := forward * input.y + right * input.x
-	var velocity := delta * speed * direction
+	var curr_speed := speed_with_shift if Input.is_key_pressed(KEY_SHIFT) else speed
+	var direction := global_transform.basis * input
+	var velocity := delta * curr_speed * direction
 	translate(velocity)
 
 func _input(event: InputEvent) -> void:
