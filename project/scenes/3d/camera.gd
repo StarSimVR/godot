057a08ee9ef4c06aad29ff3399738e0d5dcaa40a
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2022-01-12 15:28:09 +0100

    Warp points and solar system

--- project/scenes/3d/camera.gd
+++ project/scenes/3d/camera.gd
@@ -3,25 +3,36 @@ extends Camera
 export var speed := 5.0
 export var speed_with_shift := 25.0
 export var look_sensitivity := 1.0
-var _mouse_motion := Vector2()
+onready var _objects := get_node("../../Objects/Space").get_children()
+var _rotation := Vector2()
+var _free_flight_mode := false
+var _curr_object := 1
 
 func _process(delta: float) -> void:
-	transform.basis = Basis(Vector3(_mouse_motion.y * -0.001, _mouse_motion.x * -0.001, 0))
-
 	var input := Vector3()
-	if Input.is_action_pressed("move_forward"):
-		input.z -= 1
-	if Input.is_action_pressed("move_backward"):
-		input.z += 1
-	if Input.is_action_pressed("move_left"):
-		input.x -= 1
-	if Input.is_action_pressed("move_right"):
-		input.x += 1
-	if Input.is_action_pressed("move_up"):
-		input.y += 1
-	if Input.is_action_pressed("move_down"):
-		input.y -= 1
-	input = input.normalized()
+	if !_free_flight_mode:
+		var object: Spatial = _objects[_curr_object]
+		var origin := object.transform.origin
+		#look_at(origin, Vector3(0, 1, 0))
+		#rotation_degrees.y = -180
+		look_at_from_position(Vector3(origin.x, origin.y, -1.0), origin, Vector3(0, 1, 0))
+		#transform.origin = Vector3(origin.x, origin.y, -1.0)
+		#input = Vector3(0, 0, -1)
+	else:
+		transform.basis = Basis(Vector3(_rotation.y, _rotation.x, 0))
+		if Input.is_action_pressed("move_forward"):
+			input.z -= 1
+		if Input.is_action_pressed("move_backward"):
+			input.z += 1
+		if Input.is_action_pressed("move_left"):
+			input.x -= 1
+		if Input.is_action_pressed("move_right"):
+			input.x += 1
+		if Input.is_action_pressed("move_up"):
+			input.y += 1
+		if Input.is_action_pressed("move_down"):
+			input.y -= 1
+		input = input.normalized()
 
 	var curr_speed := speed_with_shift if Input.is_key_pressed(KEY_SHIFT) else speed
 	var direction := global_transform.basis * input
@@ -29,9 +40,24 @@ func _process(delta: float) -> void:
 	translate(velocity)
 
 	if Input.is_action_just_pressed("print_position"):
-		print("Translation:", transform.origin)
-		print("Basis:", transform.basis)
+		print("Position: ", transform.origin)
+		print("Rotation: ", _rotation)
+		print("Ms: ", OS.get_ticks_msec())
+	if Input.is_action_just_pressed("toggle_free_flight_mode"):
+		_free_flight_mode = !_free_flight_mode
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion && Input.is_mouse_button_pressed(BUTTON_LEFT):
-		_mouse_motion += event.relative * look_sensitivity
+		_rotation += event.relative * look_sensitivity * (-0.001)
+		_rotation.x = fmod(_rotation.x, 2 * PI)
+		_rotation.y = fmod(_rotation.y, 2 * PI)
+
+	if Input.is_action_just_pressed("prev_warp_point"):
+		_curr_object -= 1
+		if _curr_object == 0:
+			_curr_object = _objects.size() - 1
+	if Input.is_action_just_pressed("next_warp_point"):
+		_curr_object += 1
+		if _curr_object == _objects.size():
+			_curr_object = 1
+		#transform.origin = _objects[_curr_object].global_transform.origin + Vector3(0, 0, 5)
