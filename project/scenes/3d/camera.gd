433577a80419bf8bcc4796b60def8f331ef6e7b2
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2022-01-30 23:12:07 +0100

    Add class BaseCamera, add changing speed

--- project/scenes/3d/camera.gd
+++ project/scenes/3d/camera.gd
@@ -1,52 +1,40 @@
-extends Camera
+extends BaseCamera
 
 export var speed := 5.0
 export var speed_with_shift := 25.0
 export var look_sensitivity := 1.0
-onready var _objects := get_node("/root/Main/Objects/Space").get_children()
-onready var _labels := get_node("/root/Main/Objects/CanvasLayer/Labels")
 var _rotation := Vector2()
 var _free_flight_mode := false
-var _curr_object := 1
 
 func _ready() -> void:
-	for object in _objects:
-		if object.name == "sun":
-			continue
-		var label := Label.new()
-		label.name = object.name
-		label.set_text(object.name)
-		_labels.add_child(label)
+	create_labels()
 
-func _process(delta: float) -> void:
+func move_in_free_flight_mode(delta: float) -> void:
 	var input := Vector3()
-	if !_free_flight_mode:
-		var sun: Spatial = _objects[0]
-		var object: Spatial = _objects[_curr_object]
-		var origin := object.transform.origin
-		var sun_origin := sun.transform.origin
-		var position := origin + origin.normalized() * 1
-		if position != sun_origin:
-			look_at_from_position(position, sun_origin, Vector3(0, 0, 1))
-	else:
-		transform.basis = Basis(Vector3(_rotation.y, _rotation.x, 0))
-		if Input.is_action_pressed("move_forward"):
-			input.z -= 1
-		if Input.is_action_pressed("move_backward"):
-			input.z += 1
-		if Input.is_action_pressed("move_left"):
-			input.x -= 1
-		if Input.is_action_pressed("move_right"):
-			input.x += 1
-		if Input.is_action_pressed("move_up"):
-			input.y += 1
-		if Input.is_action_pressed("move_down"):
-			input.y -= 1
-		input = input.normalized()
+	if Input.is_action_pressed("move_forward"):
+		input.z -= 1
+	if Input.is_action_pressed("move_backward"):
+		input.z += 1
+	if Input.is_action_pressed("move_left"):
+		input.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input.x += 1
+	if Input.is_action_pressed("move_up"):
+		input.y += 1
+	if Input.is_action_pressed("move_down"):
+		input.y -= 1
+	input = input.normalized()
 
+	transform.basis = Basis(Vector3(_rotation.y, _rotation.x, 0))
 	var curr_speed := speed_with_shift if Input.is_key_pressed(KEY_SHIFT) else speed
 	var velocity := delta * curr_speed * input
 	translate(velocity)
+
+func _process(delta: float) -> void:
+	if _free_flight_mode:
+		move_in_free_flight_mode(delta)
+	else:
+		look_at_current_object()
 
 	if Input.is_action_just_pressed("print_position"):
 		print("Position: ", transform.origin)
@@ -54,17 +42,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_free_flight_mode"):
 		_free_flight_mode = !_free_flight_mode
 
-	for object in _objects:
-		if object.name == "sun":
-			continue
-		var label: Label = _labels.get_node(object.name)
-		var offset := Vector2(label.get_size().x / 2, 0)
-		var object_position: Vector3 = object.global_transform.origin
-		if is_position_behind(object_position):
-			label.hide()
-		else:
-			label.show()
-		label.rect_position = unproject_position(object_position) - offset
+	render_labels()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion && Input.is_mouse_button_pressed(BUTTON_LEFT):
@@ -73,10 +51,10 @@ func _input(event: InputEvent) -> void:
 		_rotation.y = fmod(_rotation.y, 2 * PI)
 
 	if Input.is_action_just_pressed("prev_warp_point"):
-		_curr_object -= 1
-		if _curr_object == 0:
-			_curr_object = _objects.size() - 1
+		prev_warp_point()
 	if Input.is_action_just_pressed("next_warp_point"):
-		_curr_object += 1
-		if _curr_object == _objects.size():
-			_curr_object = 1
+		next_warp_point()
+	if Input.is_action_just_pressed("slower"):
+		slower()
+	if Input.is_action_just_pressed("faster"):
+		faster()
---
+++ project/scenes/base_camera.gd
@@ -0,0 +1,57 @@
+class_name BaseCamera
+extends Camera
+
+onready var _objects := get_node("/root/Main/Objects/Space").get_children()
+onready var _labels := get_node("/root/Main/Objects/CanvasLayer/Labels")
+var _curr_object := 1
+
+func change_speed(k: float) -> void:
+	for object in _objects:
+		object.speed *= k
+
+func slower() -> void:
+	change_speed(0.5)
+
+func faster() -> void:
+	change_speed(2)
+
+func create_labels() -> void:
+	for object in _objects:
+		if object.name == "sun":
+			continue
+		var label := Label.new()
+		label.name = object.name
+		label.set_text(object.name)
+		_labels.add_child(label)
+
+func render_labels() -> void:
+	for object in _objects:
+		if object.name == "sun":
+			continue
+		var label: Label = _labels.get_node(object.name)
+		var offset := Vector2(label.get_size().x / 2, 0)
+		var object_position: Vector3 = object.global_transform.origin
+		if is_position_behind(object_position):
+			label.hide()
+		else:
+			label.show()
+		label.rect_position = unproject_position(object_position) - offset
+
+func look_at_current_object() -> void:
+	var sun: Spatial = _objects[0]
+	var object: Spatial = _objects[_curr_object]
+	var origin := object.transform.origin
+	var sun_origin := sun.transform.origin
+	var position := origin + origin.normalized() * 1
+	if position != sun_origin:
+		look_at_from_position(position, sun_origin, Vector3(0, 0, 1))
+
+func prev_warp_point() -> void:
+	_curr_object -= 1
+	if _curr_object == 0:
+		_curr_object = _objects.size() - 1
+
+func next_warp_point() -> void:
+	_curr_object += 1
+	if _curr_object == _objects.size():
+		_curr_object = 1
