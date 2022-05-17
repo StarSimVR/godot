9907902ed65be10c9340e6aa59b198a67988f0fa
Niklas Leukroth <niklasleukroth@gmail.com>
2022-05-17 10:41:02 +0200

    Implement trackpad functionality and comment the code

--- project/scenes/vr/VR_Controller.gd
+++ project/scenes/vr/VR_Controller.gd
@@ -5,31 +5,32 @@ const BUTTON = {
 	"TouchPad": 14,
 	"SideButton": 2
 }
-
+#Load Nodes
 onready var StarSimVRCamera := get_node("../StarSimVRCamera")
 onready var Space := get_node("/root/Main/Objects/Space")
 onready var BaseController := get_node("../Controller")
 onready var grabCast := get_node("GrabCast")
+
+#Setup const
+const CONTROLLER_DEADZONE = 0.65
+const MOVEMENT_SPEED = 0.5
+
 var isInited = false
 
-
+#Controller Movement
 var controller_velocity = Vector3(0, 0, 0)
 var prior_controller_position = Vector3(0, 0, 0)
 var prior_controller_velocities = []
+var directional_movement = false
 
+#Controller Functions
 var held_object = null
 var held_object_data = {"mode":RigidBody.MODE_RIGID, "layer":1, "mask":1}
-
 var grab_mode = "AREA"
 var teleport_pos
 var teleport_mesh
 var teleport_button_down
 
-const CONTROLLER_DEADZONE = 0.65
-
-const MOVEMENT_SPEED = 1.5
-
-var directional_movement = false
 
 func _ready():
 	#teleport_mesh = get_tree().root.get_node("Game/Teleport_Mesh")
@@ -65,7 +66,36 @@ func _physics_process(delta):
 		if prior_controller_velocities.size() > 30:
 			prior_controller_velocities.remove(0)
 	
+	#Call the trackpad function
+	_physics_process_directional_movement(delta)
 	
+	
+#Function to track the trackpad, not finished	
+func _physics_process_directional_movement(delta):
+	# Convert the VR controller's trackpad axis values into a Vector2 and store it in a variable called trackpad_vector.
+	var trackpad_vector = Vector2(-get_joystick_axis(1), get_joystick_axis(0))
+	# If the trackpad_vector's length is less than CONTROLLER_DEADZONE, then just ignore the input entirely.
+	if trackpad_vector.length() < CONTROLLER_DEADZONE:
+		trackpad_vector = Vector2(0,0)
+	# If the trackpad_vector's length is not less than CONTROLLER_DEADZONE, then process the input
+	# while accounting for the deadzones within the controller.
+	else:
+		# (See the link at CONTROLLER_DEADZONE for an explanation of how this code works!)
+		trackpad_vector = trackpad_vector.normalized() * ((trackpad_vector.length() - CONTROLLER_DEADZONE) / (1 - CONTROLLER_DEADZONE))
+	
+	var movement_vector = trackpad_vector.normalized()
+	
+	var forward_direction = get_parent().get_node("Player_Camera").global_transform.basis.z.normalized()
+	var right_direction = get_parent().get_node("Player_Camera").global_transform.basis.x.normalized()
+	
+	# Calculate the amount of movement the player will take on the Z (forward) axis and assign it to movement_forward.
+	var movement_forward = forward_direction * movement_vector.x * delta * MOVEMENT_SPEED
+	# Calculate the amount of movement the player will take on the X (right) axis and assign it to movement_forward.
+	var movement_right = right_direction * movement_vector.y * delta * MOVEMENT_SPEED
+	
+	#+++Not finished, further adjustments when functionality is needed+++
+	
+#Function to init the scene upon the first load
 func init():
 	if(self.controller_id == 2):
 		grabCast.visible = true
@@ -79,6 +109,7 @@ func init():
 	update_camera_position()
 	isInited = true;
 
+#Function to handle any physical input
 func _process(_delta):
 	if not isInited:
 		 init()
@@ -86,11 +117,14 @@ func _process(_delta):
 		var leftController = get_node("../LeftController")
 		var rightController = get_node("../RightController")
 		var rightRayCast = rightController.getRayCast()
+		#If the raycast is hitting and object, notify the user by rumbling the controller
 		if(rightRayCast.is_colliding()):
 			leftController.rumble = 0.3
 		else:
 			leftController.rumble = 0
 
+
+#Function to specify the behaviour once a button is pressed
 func button_pressed(button_index):
 	match button_index:
 		BUTTON.Trigger:
@@ -120,19 +154,24 @@ func button_pressed(button_index):
 		_:
 			print("Did not match. pressed: %d" % button_index)
 
+
+#Not yet implemented, will be relevant in the editing stage
 func button_released(button_index):
 	#print("Released: %d" % button_index)
 	pass
 	
 	
+#Function to call upon the BaseController warp point backwards functionality and update the camera
 func change_warp_point_backwards():
 	BaseController.prev_warp_point()
 	update_camera_position()
 	
+#Function to call upon the BaseController warp point forwards functionality and update the camera	
 func change_warp_point_forwards():
 	BaseController.next_warp_point()
 	update_camera_position()
 		
+#Function to specify how to update and redisplay a scene in VR
 func update_camera_position():
 	var object: Spatial = Space.get_cur_object()
 	if(object == null): return
@@ -140,11 +179,13 @@ func update_camera_position():
 	var camera_offset = get_parent().get_node("StarSimVRCamera").global_transform.origin - get_parent().global_transform.origin
 	camera_offset.y = 0
 	get_parent().global_transform.origin = origin - camera_offset
-	#StarSimVRCamera.updatePosition(origin)
 		
+
+#Function to specify what happens when the grab button on the controller is pressed
 func grab():
 	var rightRayCast = get_node("../RightController").getRayCast()
 	rightRayCast.force_raycast_update()
+	#when a collision is detected, teleport the user to the location
 	if(rightRayCast.is_colliding()):
 		var body = rightRayCast.get_collider()
 		Space.set_cur_object(body)
@@ -156,6 +197,7 @@ func sleep_area_entered(body):
 func sleep_area_exited(body):
 	pass
 	
+#Function to move the user to and entity outside of the scene, called the godView
 func change_to_god_view():
 	var object: Spatial = get_node("../../Objects/GodView")
 	var origin := object.transform.origin
