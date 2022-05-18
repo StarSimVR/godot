b3f4f5a7003194c97a53896a34c26823975d89e6
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2022-05-18 22:33:13 +0200

    Speed up/slow down rotated objects

--- project/scenes/rotate.gd
+++ project/scenes/rotate.gd
@@ -2,5 +2,11 @@ extends Spatial
 
 export(float) var speed = 1.0 # angle / sec
 
-func _process(delta):
+func _process(delta) -> void:
 	rotate_z(speed * delta)
+
+func slower() -> void:
+	speed *= 0.5
+
+func faster() -> void:
+	speed *= 2
