7a6c3a4ccdb8bf4cb31ea7e8cc35d6cada2ee2c1
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2022-06-28 21:51:00 +0200

    Editor for mass and radius with mouse picking

--- project/scenes/editor/editor.gd
+++ project/scenes/editor/editor.gd
@@ -1,31 +1,48 @@
 extends Node
 
-enum Mode {MASS_DENSITY, MASS_RADIUS}
 var scene: SceneEncoder = null
-var mode := 0
-var obj := {}
+var obj := {"not_found": true}
+var orig_mass := 0.0
 var orig_radius := 0.0
 
 func _ready():
 	scene = SceneEncoder.new("solar_system")
 
-func save():
-	var name: String = $Name.get_text()
-	var mass_radius := float($MassRadius.get_text())
-	var density_radius := float($DensityRadius.get_text())
-	var mass: float
-	var radius: float
-	var density: float
-	if mode == Mode.MASS_DENSITY:
-		mass = mass_radius
-		radius = mass_radius
-		density = density_radius
-	elif mode == Mode.MASS_RADIUS:
-		mass = mass_radius
-		radius = density_radius
-		density = mass / calc_volume(radius)
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		var camera: Camera = get_node("/root/Main/3D/Camera")
+		var from: Vector3 = camera.project_ray_origin(event.position)
+		var to: Vector3 = from + camera.project_ray_normal(event.position) * 1000
+		var result := camera.get_world().direct_space_state.intersect_ray(from, to, [], 2147483647, true, true)
+		print(event.position, from, to, result)
+		if result.empty():
+			unload_object()
+		else:
+			load_object(result.collider.get_parent().name)
+	elif Input.is_action_just_released("click") && !obj.has("not_found"):
+		var mass_change: int = $Params/Mass.get_value()
+		var radius_change: int = $Params/Radius.get_value()
+		if mass_change == 50 && radius_change == 50:
+			return
+		if $Locked.is_pressed():
+			if mass_change == 50:
+				mass_change = radius_change
+			else:
+				radius_change = mass_change
 
-	set_radius(radius)
+		calc_new_mass(mass_change)
+		calc_new_radius(radius_change)
+
+		orig_mass = get_mass()
+		orig_radius = get_radius()
+
+		$Params/Mass.set_value(50)
+		$Params/Radius.set_value(50)
+
+		save()
+
+func save():
+	var name: String = obj.name
 	scene.save("solar_system")
 
 	var space := get_node("/root/Main/Objects/Space")
@@ -35,22 +52,18 @@ func save():
 
 	var camera := get_node("/root/Main/3D/Camera")
 	camera.start()
-
 	load_object(name)
 
-func calc_volume(radius: float):
-	return (4.0 / 3) * PI * pow(radius, 3)
+func get_mass() -> float:
+	return obj.mass if "mass" in obj else 0.0
 
 func get_radius() -> float:
 	if !obj.has("scale") && !obj.has("radius"):
 		return 0.0
 	return obj.scale[0] if "with_script" in obj && obj.with_script else obj.radius
 
-func get_mass() -> float:
-	return 0.0
-
-func get_density() -> float:
-	return 0.0
+func set_mass(mass: float) -> void:
+	obj.mass = mass
 
 func set_radius(radius: float) -> void:
 	if "with_script" in obj && obj.with_script:
@@ -60,45 +73,42 @@ func set_radius(radius: float) -> void:
 
 func load_object(name: String) -> void:
 	obj = scene.get_object(name)
-	var mass_radius: float
-	var density_radius: float
-	if mode == Mode.MASS_DENSITY:
-		mass_radius = get_radius()
-		density_radius = get_density()
-	elif mode == Mode.MASS_RADIUS:
-		mass_radius = get_mass()
-		density_radius = get_radius()
-	orig_radius = mass_radius
-	#$Name.set_text(name)
-	#$MassRadius.set_value(mass_radius)
-	#$DensityRadius.set_value(density_radius)
+	orig_mass = get_mass()
+	orig_radius = get_radius()
 	update_info()
 
-func change_mode(new_mode: int):
-	mode = new_mode
-	if mode == Mode.MASS_DENSITY:
-		$MassRadiusLabel.set_text("Mass/radius: ")
-		$DensityRadiusLabel.set_text("Density: ")
-	else:
-		$MassRadiusLabel.set_text("Mass: ")
-		$DensityRadiusLabel.set_text("Radius: ")
+func unload_object() -> void:
+	obj = {"not_found": true}
+	orig_mass = 0.0
+	orig_radius = 0.0
+	update_info()
 
-func update_info():
+func update_info() -> void:
 	if "not_found" in obj:
-		$Info.set_text("Not found")
+		$Info.set_text("")
 	else:
-		var params := [get_mass(), get_radius(), get_density()]
-		$Info.set_text("mass=%.1f, radius=%.1f, density=%.1f" % params)
+		var params := [obj.name, get_mass(), get_radius()]
+		$Info.set_text("name=%s, mass=%.1f, radius=%.1f" % params)
 
-func update_mass_radius(change: int):
-	#if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		#return
-	var radius := get_radius()
+func get_change_coeff(change: int) -> float:
 	var pct := change - 50
 	pct = pct if pct < 0 else pct * 2
-	radius = orig_radius + pct / 100.0 * orig_radius
-	set_radius(radius)
+	return 1 + pct / 100.0
+
+func calc_new_mass(change: int) -> void:
+	set_mass(orig_mass * get_change_coeff(change))
+
+func calc_new_radius(change: int) -> void:
+	set_radius(orig_radius * get_change_coeff(change))
+
+func update_mass(change: int) -> void:
+	calc_new_mass(change)
+	if $Locked.is_pressed():
+		calc_new_radius(change)
 	update_info()
 
-func update_density_radius(value: int):
-	pass # Replace with function body.
+func update_radius(change: int) -> void:
+	calc_new_radius(change)
+	if $Locked.is_pressed():
+		calc_new_mass(change)
+	update_info()
