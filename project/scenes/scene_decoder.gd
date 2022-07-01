e5b57706a34c853e43695faa3f048a6fb941b876
Luka-sama <91343355+Luka-sama@users.noreply.github.com>
2022-07-01 23:58:11 +0200

    Fix: torus is a square

--- project/scenes/scene_decoder.gd
+++ project/scenes/scene_decoder.gd
@@ -29,7 +29,7 @@ func create(scene_name: String) -> void:
 		var materials := get_materials(data, textures)
 		var planet_data := get_planet_data(data)
 		var params: Array = data.config.params if "config" in data && "params" in data.config else []
-		
+
 		#create MultiMesh if stars are loaded
 		if "config" in data && "starFile" in data.config && data.config.starFile:
 			var multiStar := get_node("/root/Main/Objects/Space/Stars/MultiStar")
@@ -40,25 +40,25 @@ func create(scene_name: String) -> void:
 				index+=1
 			multiStar.multimesh.visible_instance_count = -1
 			return
-		
+
 		for object in data.objects:
 			create_object(object, geometries, materials, planet_data, params)
 	if data.has("lights"):
 		for light in data.lights:
 			create_light(light)
-			
-			
+
+
 func create_star(object: Dictionary, materials: Dictionary, index) -> void:
 	var multiStar := get_node("/root/Main/Objects/Space/Stars/MultiStar")
 	#Set the position of the star
 	var position = Transform()
 	position = position.translated(Vector3(object.position[0], object.position[1], object.position[2]))
 	multiStar.multimesh.set_instance_transform(index, position)
-	
+
 	#Set the material of the star
 	#+++Currently not working+++
 	multiStar.multimesh.set_instance_custom_data(index, materials[object.material].emission)
-	
+
 
 
 func get_textures(data: Dictionary) -> Dictionary:
@@ -261,11 +261,8 @@ func create_object(object: Dictionary, geometries: Dictionary, materials: Dictio
 	elif "centre" in object && "radius" in object && (!object.has("with_script") || !object.with_script):
 		var centre := get_randomized_vector3(object.centre, rng)
 		var radius: float = get_randomized(object.radius, rng)
-		node.transform.origin = centre + radius * Vector3(
-			cos(rng.randf_range(0, 2 * PI)),
-			sin(rng.randf_range(0, 2 * PI)),
-			0
-		)
+		var phi := rng.randf_range(0, 2 * PI)
+		node.transform.origin = centre + radius * Vector3(cos(phi), sin(phi), 0)
 	if "speed" in object && (!object.has("with_script") || !object.with_script):
 		node.set_script(rotate)
 		node.is_editor = is_editor
