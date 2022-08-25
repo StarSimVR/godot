93d8cb01775e7e75c4f08b63648d19e1927d7b9b
Niklas Leukroth <niklasleukroth@gmail.com>
2022-08-25 11:50:09 +0200

    Add help text in vr

--- project/scenes/vr/VR_Controller.gd
+++ project/scenes/vr/VR_Controller.gd
@@ -10,6 +10,7 @@ onready var StarSimVRCamera := get_node("../StarSimVRCamera")
 onready var _math_objects := get_node("/root/Main/Objects/Space/MathObjects")
 onready var BaseController := get_node("../Controller")
 onready var grabCast := get_node("GrabCast")
+onready var help_text := get_node("Controller_GUI")
 
 #Setup const
 const CONTROLLER_DEADZONE = 0.65
@@ -149,13 +150,14 @@ func button_pressed(button_index):
 				_:
 					print("TouchPad__ControllerID: %d" % self.controller_id)
 		BUTTON.SideButton:
-			match self.name:
-				"LeftController":
-					BaseController.slower()
-				"RightController":
-					BaseController.faster()
-				_:
-					print("SideButton__ControllerID: %d" % self.controller_id)
+			help_text.visible = !help_text.visible
+#			match self.name:
+#				"LeftController":
+#					#BaseController.slower()
+#				"RightController":
+#					#BaseController.faster()
+#				_:
+#					print("SideButton__ControllerID: %d" % self.controller_id)
 		_:
 			print("Did not match. pressed: %d" % button_index)
 
